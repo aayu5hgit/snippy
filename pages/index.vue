8e8315338143828517abe0f0d918d11f9d0ef46c
AAYUSH TALREJA <86314754+aayu5hgit@users.noreply.github.com>
2023-06-28 23:38:36 +0530

    Index Content

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,14 @@ useHead({
   <section class="h-screen grid place-items-center">
     <div class="max-w-3xl mx-auto text-center">
       <h1 class="heading text-5xl lg:text-6xl font-bold">
-        Make your links tiny and easy to share with tnyl
+        Snip your links and Share easily with Snippy
       </h1>
       <p class="text-lg w-10/12 md:w-2/3 mx-auto my-10">
-        Transform your long, cumbersome URLs into short and shareable links with
-        TinyLink
+        Transform your long URLs into short or custom URLs and generate shareable links with
+        Snippy URL
       </p>
       <NuxtLink :to="{ name: 'dashboard' }" class="btn text-sm"
-        >Shorten a Link</NuxtLink
+        >Snip-a-Link</NuxtLink
       >
     </div>
   </section>
